31e643feac1801ff0389bb9d1f7b0057a96a0b56
mir0-1 <stoyanov.karlovo@gmail.com>
2023-05-06 18:24:43 +0300

    fix semicolon macro issue

--- MockClient.h
+++ MockClient.h
@@ -2,8 +2,8 @@
 #include "IntentionalFailureSupportedObject.h"
 #include <NetworkManager.h>
 
-#define MOCK_VALID_CLIENT (NMClient*)0x1;
-#define MOCK_INVALID_CLIENT (NMClient*)0x0;
+#define MOCK_VALID_CLIENT (NMClient*)0x1
+#define MOCK_INVALID_CLIENT (NMClient*)0x0
 
 class MockClient : public IntentionalFailureSupportedObject
 {
